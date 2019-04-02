192ced1b3cfc367abd3521d290c9afb960051461
Kalaiselwen Jayabalan <Kalaiselwen.DTK.Jayabalan@lystkom.se>
2019-04-02 11:14:57 +0200

    halfway done

--- ThiccGames/MainWindow.xaml.cs
+++ ThiccGames/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ThiccGames
 {
@@ -20,9 +10,38 @@ namespace ThiccGames
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+        //saves the current results of box in game
+        private MarkType[] mResults;
+        private bool mPlayer1Turn;//true if player 1 (X) or player 2 turn (O)
+        private bool mGameEnded;//true when player X/O wins 
+        #endregion
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+        #endregion
+
+        private void NewGame()
+        {
+            mResults = new MarkType[9];
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+
+            mPlayer1Turn = true;
+            //inerate all buttons
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                button.Content = string.Empty;
+            });
         }
     }
 }
---
+++ ThiccGames/MarkType.cs
@@ -0,0 +1,9 @@
+﻿namespace ThiccGames
+{
+    public enum MarkType //shows where the current posision of the box is in the game
+    {
+        Free, //open
+        Nought,// O locked.
+        Cross // X locked.
+    }
+}
